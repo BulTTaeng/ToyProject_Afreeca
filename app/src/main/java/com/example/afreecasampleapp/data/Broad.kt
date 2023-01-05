80fda6fc9b69a1b7a0cb354770a1c59e802524d7
Choi-JaeHyeok-21500749 <wogur7496@gmail.com>
2023-01-05 13:06:59 +0900

    Feat: Detail Page detail page xml 추가

--- app/src/main/java/com/example/afreecasampleapp/data/Broad.kt
+++ app/src/main/java/com/example/afreecasampleapp/data/Broad.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Broad (
     @field:SerializedName("broad_title") val broad_title: String,
     @field:SerializedName("broad_cate_no") val broad_cate_no : Int,
+    @field:SerializedName("user_id") val user_id: String,
     @field:SerializedName("user_nick") val user_nick: String,
     @field:SerializedName("profile_img") val profile_img: String,
     @field:SerializedName("broad_thumb") val broad_thumb: String,
