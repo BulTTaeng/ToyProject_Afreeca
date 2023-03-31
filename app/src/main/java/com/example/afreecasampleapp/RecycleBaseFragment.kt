0addc3932692de5047b160dc44c0b2f995483924
Choi-JaeHyeok-21500749 <wogur7496@gmail.com>
2023-03-31 14:27:08 +0900

    Feat: stateflow 추후 방송리스트들의 정보들이 다른 구독자가 생길 수 도 있다. 따라서 그냥 변수 보다는 stateflow에 데이터를 저장하는 것이 확장성의 이점을 줄 수 있다고 판단 따라서 stateflow로 data holder 역할 수행

--- app/src/main/java/com/example/afreecasampleapp/RecycleBaseFragment.kt
+++ app/src/main/java/com/example/afreecasampleapp/RecycleBaseFragment.kt
@@ -70,8 +70,10 @@ abstract class RecycleBaseFragment<T: ViewDataBinding>(@LayoutRes private val la
     fun getCategoryBroads(tapId: Int) {
         broadListJob?.cancel()
         broadListJob = lifecycleScope.launch {
-            viewModel.getBroadList(tapId).collectLatest {
-                adapter.submitData(it)
+            viewModel.getBroadList(tapId)?.collectLatest {
+                it?.let{
+                    adapter.submitData(it)
+                }
             }
         }
     }
